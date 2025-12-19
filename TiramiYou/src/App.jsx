01f344d9fd2481a836e28aec4cce21501aee231e
Ayman <ayman1@outlook.de>
2025-12-19 22:06:44 +0100

    Update

--- TiramiYou/src/App.jsx
+++ TiramiYou/src/App.jsx
@@ -1,20 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+// Seiten fürs Routing
+import TiramiYou from './pages/Home.jsx';
+import Produkte from './pages/Produkte.jsx';
+import Story from './pages/Story.jsx';
+import Kontakt from './pages/Kontakt.jsx';
+import NotFound from './pages/NotFound.jsx';
+import News from './pages/News.jsx';
+import Maps from './pages/Maps.jsx';
+import Anfahrt from "./Anfahrt.jsx";
+
+
+// Globale Komponenten
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
-import Anfahrt from "./Anfahrt.jsx";
+
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-grow pt-28"> {/* flex-grow nimmt allen verfügbaren Platz */}
-        <Anfahrt />
-      </main>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col">
+
+        {/* Header bleibt oben */}
+        <Header />
+
+        {/* Inhalt wächst dynamisch */}
+        <main className="flex-grow pt-28">
+
+          {/* Routing Bereich */}
+          <Routes>
+            <Route path="/" element={<TiramiYou />} />
+            <Route path="/produkte" element={<Produkte />} />
+            <Route path="/story" element={<Story />} />
+            <Route path="/kontakt" element={<Kontakt />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/anfahrt" element={<Anfahrt />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+
+        {/* Footer bleibt unten */}
+        <Footer />
+
+      </div>
+    </BrowserRouter>
   );
 }
 
